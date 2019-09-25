92d01016fc3665dda6113a9324ed6220f68305d9
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-09-25 17:42:41 +0200

    app sync graphql

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator } from "aws-amplify-react";
+import { API, graphqlOperation } from "aws-amplify";
+
+const ListTodos = `
+  query {
+    listTodos{
+      items{
+        id description name completed
+      }
+    }
+  }
+
+`;
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    async function getTodos() {
+      const todoData = await API.graphql(graphqlOperation(ListTodos));
+      setTodos(todoData.data.listTodos.items);
+    }
+    getTodos();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +40,12 @@ function App() {
           Learn React
         </a>
       </header>
+      {todos.map((todo, i) => (
+        <div>
+          <h3>{todo.name}</h3>
+          <p>{todo.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
