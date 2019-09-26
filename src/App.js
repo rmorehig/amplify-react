9c9c0cf7e627a14c068d670ab2d1eb4be468d156
Rafael Moreno Higueras <rmh1516@gmail.com>
2019-09-26 15:09:50 +0200

    testing s3 storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import { withAuthenticator } from "aws-amplify-react";
-import { API, graphqlOperation } from "aws-amplify";
+import { API, graphqlOperation, Storage } from "aws-amplify";
 
 const ListTodos = `
   query {
@@ -16,24 +15,53 @@ const ListTodos = `
 `;
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([{ name: "no todos" }]);
+  const [file, setFile] = useState([{ fileUrl: "", file: "", fileName: "" }]);
 
-  useEffect(() => {
-    async function getTodos() {
-      const todoData = await API.graphql(graphqlOperation(ListTodos));
-      setTodos(todoData.data.listTodos.items);
-    }
-    getTodos();
-  }, []);
+  const updateTodos = async () => {
+    const todoData = await API.graphql(graphqlOperation(ListTodos));
+    setTodos(todoData.data.listTodos.items);
+  };
+
+  const getFile = async () => {
+    Storage.get("avatar.JPG")
+      .then(data => {
+        setFile({ fileUrl: data });
+      })
+      .catch(err => {
+        console.log("error fetching image!");
+      });
+  };
+
+  const handleChange = e => {
+    const file = e.target.files[0];
+    setFile({ fileUrl: URL.createObjectURL(file), file, fileName: file.name });
+  };
+
+  const saveFile = () => {
+    Storage.put(file.fileName, file.file)
+      .then(() => {
+        console.log("succesfully saved file!");
+        setFile([{ fileUrl: "", file: "", fileName: "" }]);
+      })
+      .catch(err => {
+        console.log("error uploading file!");
+      });
+  };
 
   return (
     <div className="App">
+      <button onClick={updateTodos}>Get todos</button>
       {todos.map((todo, i) => (
         <div key={i}>
           <h3>{todo.name}</h3>
           <p>{todo.description}</p>
         </div>
       ))}
+      <input type="file" onChange={handleChange} />
+      <img src={file.fileUrl} alt="no file uploaded" />
+      <button onClick={saveFile}>Save file</button>
+      <button onClick={getFile}>Get file</button>
     </div>
   );
 }
